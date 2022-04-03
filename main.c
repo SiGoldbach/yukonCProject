4072cdf1391d9125d418f57cbdf01ce82ed35983
Valdemar Nielsen <v.nielsen@live.dk>
2022-04-03 18:11:51 +0200

    Added new random sort method and possibility to make new file and copy the current deck into said file.

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <intrin.h>
 
 void SI(int split, char *cardDeck);
+
+void SD(char filename[], char *cardDeck);
+
+void SR(char *cardDeck);
 
 int main() {
     FILE *inStream;
@@ -17,11 +23,17 @@ int main() {
         counter = counter + 2;
 
     }
+    // calls on the different methods available;
+    SR(cards);
+    printf("\n");
+    SD("cards.txt", cards);
+    /*
     SI(100, cards);
     printf("\n");
-
+*/
     int cardPrintedNUm = 0;
     int rowsPrinted = 0;
+    // prints out the first line containing the different
     printf("C1\tC2\tC3\tC4\tC5\tC6\tC7\n\n");
     for (int j = 0; j <= 104; ++j) {
         printf("%c", cards[j]);
@@ -44,6 +56,7 @@ int main() {
     return 0;
 }
 
+// method to split card into smaller stacks and sort them back into one deck in order of 1 by 1 from each deck.
 void SI(int split, char *cardDeck) {
     if (split > 52 || split < 0) {
         printf("you entered invalid card deck split-size\n");
@@ -63,13 +76,52 @@ void SI(int split, char *cardDeck) {
     for (int i = 0; i < 104; i += 2) {
 
         if ((i % 4 == 0 && removedCardDeck1 < split) || removedCardDeck2 >= (52 - split)) {
-            cardDeck[i] = deck1[removedCardDeck1*2];
-            cardDeck[i+1] = deck1[removedCardDeck1*2+1];
+            cardDeck[i] = deck1[removedCardDeck1 * 2];
+            cardDeck[i + 1] = deck1[removedCardDeck1 * 2 + 1];
             removedCardDeck1++;
         } else {
-            cardDeck[i] = deck2[removedCardDeck2*2];
-            cardDeck[i+1] = deck2[removedCardDeck2*2+1];
+            cardDeck[i] = deck2[removedCardDeck2 * 2];
+            cardDeck[i + 1] = deck2[removedCardDeck2 * 2 + 1];
             removedCardDeck2++;
         }
     }
+}
+
+void SR(char *cardDeck) {
+    for (int i = 0; i < 52; i++) {
+        char cardToShuffle = cardDeck[i * 2];
+        char cardToShuffle2 = cardDeck[i * 2 + 1];
+        int randomCardLocation = (rand() % (i + 1)) * 2;
+        char currentVal = cardDeck[randomCardLocation];
+        char currentFaction = cardDeck[randomCardLocation + 1];
+        char nextVal;
+        char nextFaction;
+        for (int j = randomCardLocation / 2; j < i; j++) {
+            nextVal = cardDeck[j * 2 + 2];
+            nextFaction = cardDeck[j * 2 + 3];
+            cardDeck[j * 2 + 2] = currentVal;
+            cardDeck[j * 2 + 3] = currentFaction;
+            currentVal = nextVal;
+            currentFaction = nextFaction;
+        }
+        cardDeck[randomCardLocation] = cardToShuffle;
+        cardDeck[randomCardLocation + 1] = cardToShuffle2;
+
+    }
+}
+// makes file named cards and prints the current deck.
+void SD(char filename[], char *cardDeck) {
+    FILE *f;
+    if (strlen(filename) == 0) {
+        f = fopen("cards.txt", "wb");
+    } else {
+        f = fopen(filename, "wb");
+    }
+    fputs("Current card deck\n", f);
+    for (int i = 0; i < 104; i += 2) {
+        fprintf(f,"%c",cardDeck[i]);
+        fprintf(f, "%c\n", cardDeck[i+1]);
+
+    }
+    fclose(f);
 }
